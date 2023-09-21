cc69a4731c6960ff991feb1a7dc29bd291ff83df
“luissantos” <luissantosbh@gmail.com>
2023-09-21 09:49:10 -0300

    Criação de projeto MinhaCativa

---
+++ MinhaCativa/MinhaCativaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinhaCativaApp.swift
+//  MinhaCativa
+//
+//  Created by Luís Santos on 21/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MinhaCativaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
